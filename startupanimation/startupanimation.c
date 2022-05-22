31190274b769669d4db854ad49895e2fa8ba4a38
Quyet Luu <quyet.luuhuu@gmail.com>
2022-05-22 16:03:18 +0700

    Update delay time

--- startupanimation/startupanimation.c
+++ startupanimation/startupanimation.c
@@ -7,10 +7,10 @@ int main ( void )
     int i = 1;
     char cmd[255];
     system ( "display_image -zorder=1 -noinit=1 -display=1 -size=1200x1920 -file=/nfs/images/Splash_0.png &" );
-    for ( i = 1; i <= 8; i++ ) {
+    for ( i = 1; i < 9; i++ ) {
       sprintf ( cmd, "display_image -zorder=1 -noinit=1 -display=1 -pos=0,0 -size=1200x450 -file=/nfs/images/Splash_%d.png &", i );
       system ( cmd );
-      usleep ( 70000 );
+      usleep ( 90000 );
     }
 
     system ( "slay -f display_image" );
